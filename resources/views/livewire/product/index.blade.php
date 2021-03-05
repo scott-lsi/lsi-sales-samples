6a6db06cfd4d239e626c8644a2c9ab99136866e5
Scott Brown <sbrown@lsi.co.uk>
2021-03-05 15:25:14 +0000

    Update index.blade.php

--- resources/views/livewire/product/index.blade.php
+++ resources/views/livewire/product/index.blade.php
@@ -1,5 +1,5 @@
 <div class="py-12">
-    @if(Auth::check() || session()->has('token') && Carbon::now() < Token::where('token', session('token')->token)->first()->expires){
+    @if(Auth::check() || session()->has('token') && Carbon::now() < Token::where('token', session('token')->token)->first()->expires)
         @push('pagetitle', 'Products')
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
